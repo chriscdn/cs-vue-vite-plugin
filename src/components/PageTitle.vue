36cc39acfb512cbfb6d5c1d5c784f3536b968f5f
Christopher Meyer <chris@schwiiz.org>
2021-09-20 17:56:48 +0200

    project setup

--- src/components/PageTitle.vue
+++ src/components/PageTitle.vue
@@ -1,8 +1,8 @@
 <template>
     <h1>
-        {{ title
-        }}<span v-if="subtitle"
-            >: <span class="rhmuted">{{ subtitle }}</span></span
+        {{ title }}
+        <span v-if="subtitle">
+            : <span class="rhmuted">{{ subtitle }}</span></span
         >
     </h1>
 </template>
