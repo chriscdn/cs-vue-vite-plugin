7d3fbef5733f293059c018713fce5dfef1a2267a
Christopher Meyer <chris@schwiiz.org>
2021-10-04 17:31:01 +0200

    minify off

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,14 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
-
 const path = require('path')
 
+//
+// https://vitejs.dev/guide/build.html#library-mode
+//
 export default defineConfig({
     plugins: [vue()],
     build: {
+        minify: false,
         lib: {
             entry: path.resolve(__dirname, 'src/index.js'),
             name: 'CSVueVitePlugin',
